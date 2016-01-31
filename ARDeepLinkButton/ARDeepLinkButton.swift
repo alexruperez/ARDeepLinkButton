d3a5fb482c9849786b1c7982f4d38ea2fcab0846
alexruperez <alexruperez@gmail.com>
2016-01-31 13:17:46 +0100

    First Commit

---
+++ ARDeepLinkButton/ARDeepLinkButton.swift
@@ -0,0 +1,13 @@
+//
+//  ARDeepLinkButton.swift
+//  ARDeepLinkButton
+//
+//  Created by alexruperez on 31/1/16.
+//  Copyright © 2016 alexruperez. All rights reserved.
+//
+
+import UIKit
+
+class ARDeepLinkButton: UIButton {
+
+}
---
+++ ARDeepLinkButtonExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARDeepLinkButtonExample
+//
+//  Created by alexruperez on 31/1/16.
+//  Copyright © 2016 alexruperez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
