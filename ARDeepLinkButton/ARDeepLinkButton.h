2acb58ada225972dd65b46722a155c0eab7cb3f3
alexruperez <alexruperez@gmail.com>
2016-01-31 13:02:10 +0100

    Initial Commit

---
+++ ARDeepLinkButton/ARDeepLinkButton.h
@@ -0,0 +1,19 @@
+//
+//  ARDeepLinkButton.h
+//  ARDeepLinkButton
+//
+//  Created by alexruperez on 31/1/16.
+//  Copyright © 2016 alexruperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARDeepLinkButton.
+FOUNDATION_EXPORT double ARDeepLinkButtonVersionNumber;
+
+//! Project version string for ARDeepLinkButton.
+FOUNDATION_EXPORT const unsigned char ARDeepLinkButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARDeepLinkButton/PublicHeader.h>
+
+
